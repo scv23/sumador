43155589f160af105ca7dc3a0b0b80aecb0825bc
= <=>
2022-05-27 20:46:28 +0200

    Método main creado y funcional

---
+++ sumador/src/sumador/scvFunciona.java
@@ -0,0 +1,11 @@
+package sumador;
+
+public class scvFunciona {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+		ASumar sumi = new ASumar();
+		System.out.println(sumi.total(3433));
+	}
+
+}
