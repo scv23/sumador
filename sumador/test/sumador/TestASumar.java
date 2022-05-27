7bd68f78ac702e941c3337f4c356fb0b7e748b27
= <=>
2022-05-27 19:51:14 +0200

    Preparación

---
+++ sumador/test/sumador/TestASumar.java
@@ -0,0 +1,14 @@
+package sumador;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestASumar {
+
+	@Test
+	void test() {
+		fail("No implementado aun");
+	}
+
+}
